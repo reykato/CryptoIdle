470dbd0d36b9ef61a97ccd2c2e9227cce9ffa766
tyty4 <tyty4@DESKTOP-6E9L6EM>
2021-11-05 13:59:37 -0400

    creation of CryptoIdle project

---
+++ CryptoIdle/src/cryptoidle/CryptoIdle.java
@@ -0,0 +1,13 @@
+/*
+Tyler Lindsay
+Crypto Idle
+2021-11-5
+Main class 
+ */
+package cryptoidle;
+
+public class CryptoIdle {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }    
+}
