08ec208286c29980464e0978854d9a0dc816ee8c
Wolfrum5150 <79181893+Wolfrum5150@users.noreply.github.com>
2021-12-12 21:23:16 -0500

    Added Loading Capabilities
    Test save file added as well

--- CryptoIdle/SaveHandler.java
+++ CryptoIdle/SaveHandler.java
@@ -1,26 +1,61 @@
 
 
 import java.io.*;
+import java.util.*;
 
 
 public class SaveHandler{
+    private final int TOTAL_TERMS = 8;
     
-    
-    private long timeInSeconds;
-    private Upgrade upgrades[];
+    private long timeInMillis;
+    private Upgrade upgrades[] = new Upgrade[(TOTAL_TERMS-2)];
     private double balance;
     private File save = new File("save.txt");
     
     
     public SaveHandler(Upgrade upgrades[], double balance){
        this.upgrades = upgrades;
-       timeInSeconds = System.currentTimeMillis();
     }
     
-    public SaveHandler() throws IOException{
+    public SaveHandler() throws IOException, NumberFormatException{
         BufferedReader input = new BufferedReader(new FileReader(save));
-        
+        String terms[] = new String[TOTAL_TERMS];
+        for (int i = 0; i < TOTAL_TERMS; i++){
+           terms[i] = input.readLine(); 
+        }
+        timeInMillis = Long.parseLong(terms[0]);
+        balance = Double.parseDouble(terms[1]);
+        for (int i = 2; i < TOTAL_TERMS; i++){
+            upgrades[i-2] = parseUpgrade(terms[i]);
+        }
     }
     
     
+    private Upgrade parseUpgrade(String saveFormat){
+        double mult, basePrice, revenue;
+        int amtOwned;
+        String name;
+        String numbers[] = saveFormat.split(",");
+        
+        mult = Double.parseDouble(numbers[0]);
+        basePrice = Double.parseDouble(numbers[1]);
+        revenue = Double.parseDouble(numbers[2]);
+        amtOwned = (int)Double.parseDouble(numbers[3]);
+        name = numbers[4];
+        return new Upgrade(mult, basePrice, revenue, amtOwned, name);
+    }
+    
+    
+    
+    public double getBalance(){
+        return balance;
+    }
+    
+    public long getTimeStamp(){
+        return timeInMillis;
+    }
+    
+    public String getUpgrade(int i){
+        return String.format("Amount owned: %d\nRevenue: %.2f\n", upgrades[i].getQuantity(), upgrades[i].getRev()); 
+    }
 }
