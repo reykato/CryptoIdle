f7b24dcb4001cf5064dde644de67c411c420c4a2
tyty4646 <tyty464646@gmail.com>
2021-12-12 22:33:43 -0500

    Changed UIElements and Upgrade
    Moved drawUpgrade() to UIElements from Upgrade

--- CryptoIdle/UIElements.java
+++ CryptoIdle/UIElements.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package cryptoidle;
 
 import java.awt.GridLayout;
 import java.util.*;
@@ -12,6 +13,8 @@ import javax.swing.JPanel;
 import java.awt.*;
 import javax.swing.JLabel;
 import java.awt.event.*;
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
 
 
 public class UIElements extends JFrame {
@@ -22,8 +25,12 @@ public class UIElements extends JFrame {
         super();
         this.upgdArr = upgdArr;
         this.balance = balance;
+        run();
+    }
+    
+    public void run() {
         JFrame frame = new JFrame("test");
-        frame.setSize(400, 600);
+        frame.setSize(330, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(new GridLayout(7,0));
         frame.setVisible(true);
@@ -36,19 +43,92 @@ public class UIElements extends JFrame {
         frame.add(bal);
         
         for (var upgrade : upgdArr) {
-            frame.getContentPane().add(upgrade.drawUpgrade());
+            frame.getContentPane().add(drawUpgrade(upgrade));
         }
+        
         ActionListener taskPerformer = new ActionListener() {
             @Override
                 public void actionPerformed(ActionEvent e) {
-                    repaint();
+                    frame.invalidate();
+                    frame.validate();
+                    frame.repaint();
                     System.out.println("updated");
             }
         };
         new Timer(500, taskPerformer).start();
     }
+    
+    public double getBalance() {
+        return balance;
+    }
+    
+    public void setBalance(double in) {
+        balance = in;
+    }
         
     public Upgrade[] getUpgradeArray() {
         return upgdArr;
+    }
+    public JPanel drawUpgrade(Upgrade upgrade) {        
+        Font f = new Font("Display", Font.BOLD, 20);
+        MyPanel p = new MyPanel();
+        p.setLayout(null);
+        p.setSize(300, 80);
+        JLabel nameL, revL, cost;
+        JButton buyButton;
+        
+        // name label
+        nameL = new JLabel(upgrade.getName());
+        nameL.setFont(f);
+        nameL.setBounds(84, 4, 240, 25);
+        
+        buyButton = new JButton("Buy");
+        buyButton.setBounds(4, 4, 70, 50);
+        buyButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("button pressed");
+                
+                balance -= upgrade.buy(balance, 1);
+                System.out.println(balance);
+                System.out.println(upgrade.getCost());
+            }
+        });
+        
+        
+        // i am unsure if revenue should be displayed
+        /*
+        revL = new JLabel(String.valueOf(revenue));
+        revL.setHorizontalAlignment(SwingConstants.LEFT);
+        revL.setFont(f);
+        revL.setBounds(4, 34, 50, 25);
+        */
+        // cost label
+        cost = new JLabel(Double.toString(upgrade.getCost()));
+        cost.setFont(f);
+        cost.setBounds(84, 34, 240, 25);
+        
+        p.add(buyButton);
+        p.add(nameL);
+        //p.add(revL);
+        p.add(cost);
+        return p;        
+    }
+
+
+}
+
+
+class MyPanel extends JPanel { // create custom panel constructor to draw a rectangle to contain the upgrade
+    void drawRectangles(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        
+        g2d.setStroke(new BasicStroke(2));
+        g2d.drawRect(1, 1, 300, 60);
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        drawRectangles(g);
     }
 }
--- CryptoIdle/Upgrade.java
+++ CryptoIdle/Upgrade.java
@@ -6,16 +6,12 @@ Crypto Idle
 Upgrades class with constructor
  */
 
-import java.lang.Math;
-import javax.swing.*;
-import java.awt.*;
+package cryptoidle;
 
 public class Upgrade {
     private final double mult, basePrice, revenue;
     private int amtOwned;
-    private final String name;
-    private String costFormat;
-    
+    private final String name;    
     
     public Upgrade(double mult, double basePrice, double revenue, int amtOwned, String name) {
         this.mult = mult;
@@ -37,22 +33,13 @@ public class Upgrade {
     }
     
     public double buy(double balance, int quantity){
-        double cost = calcCost(quantity);
+        double cost = calcCost(amtOwned + quantity);
         if (balance >= cost){
             amtOwned += quantity;
-            updateString();
             return cost;
         }
         return 0;
     }
-    
-    
-    private void updateString(){
-        costFormat = String.format("%4.2f", calcCost(0));
-    }
-    
-    // Get methods
-    
     public int getQuantity(){
         return amtOwned;
     }
@@ -60,60 +47,11 @@ public class Upgrade {
     public double getRev(){
         return revenue;
     }  
-    
-    
-    
-    // Draw method
-    // TODO: Add a way to repaint the panel to update the price
-    public JPanel drawUpgrade() {        
-        Font f = new Font("Display", Font.BOLD, 20);
-        MyPanel p = new MyPanel();
-        p.setLayout(null);
-        p.setSize(300, 80);
-        JLabel nameL, revL, cost;
-        
-        
-        // name label
-        nameL = new JLabel(name);
-        nameL.setFont(f);
-        nameL.setBounds(4, 4, 240, 25);
-        
-        
-        // i am unsure if revenue should be displayed
-        /*
-        revL = new JLabel(String.valueOf(revenue));
-        revL.setHorizontalAlignment(SwingConstants.LEFT);
-        revL.setFont(f);
-        revL.setBounds(4, 34, 50, 25);
-        */
-        
-        costFormat = String.format("%4.2f", calcCost(0)); // temporarily set the 10x multiplier true for testing
-        
-        // cost label
-        cost = new JLabel(costFormat);
-        cost.setFont(f);
-        cost.setBounds(4, 34, 240, 25);
-        
-        p.add(nameL);
-        //p.add(revL);
-        p.add(cost);
-        return p;        
+    public String getName() {
+        return name;
     }
-
-}
-
-
-class MyPanel extends JPanel { // create custom panel constructor to draw a rectangle to contain the upgrade
-    void drawRectangles(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
-        
-        g2d.setStroke(new BasicStroke(2));
-        g2d.drawRect(1, 1, 250, 60);
+    public double getCost() {
+        return calcCost(amtOwned);
     }
     
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-        drawRectangles(g);
-    }
 }
